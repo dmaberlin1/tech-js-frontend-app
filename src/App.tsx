c1a24721f50f6edd433c79b2998d31b0d9c65afb
dmaberlin1 <dmaberlin77@gmail.com>
2023-01-24 19:35:03 +0200

    init: theme_provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import Home from "./components/home";
-
 import {Routes, Route} from "react-router-dom";
 import PrivateRoute from "./utils/router/privateRoute";
 import AuthRootComponent from "./components/auth";
+import {useMode,ColorModeContext} from "./theme";
+import {CssBaseline,ThemeProvider} from '@mui/material'
 
 function App() {
+    const[theme,colorMode]=useMode()
+
     return (
-        <div className="app">
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline></CssBaseline>
+                <div className="App">
+                <Routes>
+                    <Route element={<PrivateRoute/>}>
+                        <Route path={'/'} element={<Home/>}/>
+                    </Route>
+                    <Route path={'login'} element={<AuthRootComponent/>}></Route>
+                    <Route path={'register'} element={<AuthRootComponent/>}></Route>
 
-            <Routes>
+                </Routes>
+            </div></ThemeProvider>
 
-                <Route element={<PrivateRoute/>}>
-                    <Route path={'/'} element={<Home/>}/>
-                </Route>
-                <Route path={'login'} element={<AuthRootComponent/>}></Route>
-                <Route path={'register'} element={<AuthRootComponent/>}></Route>
-
-            </Routes>
-        </div>
+        </ColorModeContext.Provider>
     );
 }
 
