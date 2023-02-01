29830f9e3e3d655e4fbf91a9ec2760ab07810504
dmaberlin1 <dmaberlin77@gmail.com>
2023-02-01 11:14:01 +0200

    feat:Fetch data from coingecko -Create fetch logic -Save data to store -Get data from store in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
+import {useAppDispatch, useAppSelector} from "../../utils/hook";
+import {getFavoriteAssets} from "../../store/thunks/assets";
+import {Box, Grid} from "@mui/material";
+import {useStyles} from "./styles";
+import {current} from "@reduxjs/toolkit";
 
 const Home = () => {
+    const classes=useStyles()
+    const favoriteAssets:any[]=useAppSelector(state=>state.assets.favoriteAssets)
+    const dispatch=useAppDispatch()
+    const fetchDataRef=useRef(false)
+
+    const favoriteAssetName=useMemo(()=>['bitcoin','ethereum'],[])
+    const filteredArray=favoriteAssets.filter(
+        (value, index, self)=>index===self.findIndex((t)=>t.name===value.name)
+    )
+
+    const fetchData=useCallback((data:string[])=>{
+        data.forEach((item:string)=>{
+            dispatch(getFavoriteAssets(item))
+        })
+    },[dispatch])
+
+    // console.log(favoriteAssets,'Assets<<< ' )
+    useEffect(()=>{
+        if(fetchDataRef.current) return
+        fetchDataRef.current=true
+      fetchData(favoriteAssetName)
+    },[favoriteAssetName,fetchData])
+
+    const renderFavoriteBlock=filteredArray.map((item:any)=>{
+        console.log(item,'item<<<')
+        const currentPrice=item.data.prices[0]
+        const currentCap=item.data.market_caps[0]
+        return (
+            <Grid item lg={6} sm={6} xs={12}>
+               <Grid container className={classes.topCardItem}>
+                   <Grid item xs={12} sm={6} lg={6}>
+                       <h3 className={classes.assetName}>{item.name}</h3>
+                   <div className={classes.itemDetails}>
+                       <h3 className={classes.cardPrice}>    {currentPrice[1].toFixed(2)}</h3>
+                           <p className={classes.cardCapitalize}>{currentCap[1].toFixed(0)}</p>
+                   </div>
+                   </Grid>
+                   <Grid item xs={12} sm={6} lg={6}>
+                       <h5>Chart</h5>
+                   </Grid>
+
+               </Grid>
+            </Grid>
+        )
+    })
+
     return (
-        <div>
-            <h1>This is home page</h1>
-        </div>
+        <Box className={classes.root}>
+
+            <Grid container spacing={2}>
+                {renderFavoriteBlock}
+            </Grid>
+        </Box>
     );
 };
 
