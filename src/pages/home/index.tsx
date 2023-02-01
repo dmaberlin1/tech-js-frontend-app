0aa44abe085b861b6d89c808fc001094d0ed29ae
dmaberlin1 <dmaberlin77@gmail.com>
2023-02-01 15:06:09 +0200

    feat:Improve area chart component -Fix array length from data -Improve option chart -Add trend to change price

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,51 +5,75 @@ import {Box, Grid} from "@mui/material";
 import {useStyles} from "./styles";
 import {current} from "@reduxjs/toolkit";
 import AreaChart from "../../components/charts/area-chart";
+import TrendUp from '../../assets/images/chart/trend-up.svg'
+import TrendDown from '../../assets/images/chart/trend-down.svg'
 
-const Home:FC = ():JSX.Element => {
-    const classes=useStyles()
-    const favoriteAssets:any[]=useAppSelector(state=>state.assets.favoriteAssets)
-    const dispatch=useAppDispatch()
-    const fetchDataRef=useRef(false)
 
-    const favoriteAssetName=useMemo(()=>['bitcoin','ethereum'],[])
-    const filteredArray=favoriteAssets.filter(
-        (value, index, self)=>index===self.findIndex((t)=>t.name===value.name)
+const Home: FC = (): JSX.Element => {
+    const classes = useStyles()
+    const favoriteAssets: any[] = useAppSelector(state => state.assets.favoriteAssets)
+    const dispatch = useAppDispatch()
+    const fetchDataRef = useRef(false)
+
+    const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], [])
+    const filteredArray = favoriteAssets.filter(
+        (value, index, self) => index === self.findIndex((t) => t.name === value.name)
     )
 
-    const fetchData=useCallback((data:string[])=>{
-        data.forEach((item:string)=>{
+    const fetchData = useCallback((data: string[]) => {
+        data.forEach((item: string) => {
             dispatch(getFavoriteAssets(item))
         })
-    },[dispatch])
+    }, [dispatch])
 
     // console.log(favoriteAssets,'Assets<<< ' )
-    useEffect(()=>{
-        if(fetchDataRef.current) return
-        fetchDataRef.current=true
-      fetchData(favoriteAssetName)
-    },[favoriteAssetName,fetchData])
+    useEffect(() => {
+        if (fetchDataRef.current) return
+        fetchDataRef.current = true
+        fetchData(favoriteAssetName)
+    }, [favoriteAssetName, fetchData])
 
-    const renderFavoriteBlock=filteredArray.map((item:any)=>{
+    const renderFavoriteBlock = filteredArray.map((item: any) => {
 
         // console.log(item,'item<<<')
-        const currentPrice=item.data.prices[0]
-        const currentCap=item.data.market_caps[0]
+        const currentPrice = item.singleAsset.map(
+            (item: any) => item.current_price,
+        )
+        // const currentCap=item.singleAsset.map(
+        //     (item:any)=>item.market_cap,
+        // )
+        const changePrice = item.singleAsset.map(
+            (item: any) => item.price_change_percentage_24h,
+        )
         return (
             <Grid item lg={6} sm={6} xs={12} key={item.name}>
-               <Grid container className={classes.topCardItem}>
-                   <Grid item xs={12} sm={6} lg={6}>
-                       <h3 className={classes.assetName}>{item.name}</h3>
-                   <div className={classes.itemDetails}>
-                       <h3 className={classes.cardPrice}>    {currentPrice[1].toFixed(2)}</h3>
-                           <p className={classes.cardCapitalize}>{currentCap[1].toFixed(0)}</p>
-                   </div>
-                   </Grid>
-                   <Grid item xs={12} sm={6} lg={6}>
-                       <AreaChart data={item.data.prices}/>
-                   </Grid>
+                <Grid container className={classes.topCardItem}>
+                    <Grid item xs={12} sm={6} lg={6}>
+                        <h3 className={classes.assetName}>{item.name}</h3>
+                        <div className={classes.itemDetails}>
+                            <h3 className={classes.cardPrice}> ${currentPrice}</h3>
+                            <Box
+                                className={
+                                changePrice>0
+                                    ? `${classes.priceTrend} ${classes.trendUp}`
+                                    : `${classes.priceTrend} ${classes.trendDown}`
+                            }
+                            >
+                                {changePrice > 0 ? (
+                                        <img src={TrendUp} alt="TrendUp"/>
+                                    )
+                                    : (
+                                        <img src={TrendDown} alt="TrendDown"/>
+                                    )}
+                                <span>{Number(changePrice).toFixed(2)}%</span>
+                            </Box>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={6}>
+                        <AreaChart data={item.data}/>
+                    </Grid>
 
-               </Grid>
+                </Grid>
             </Grid>
         )
     })
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -21,25 +21,37 @@ export const useStyles=makeStyles((theme:Theme)=>{
           height:'100%',
           flexDirection:'column',
           justifyContent:'flex-end',
-          paddingBottom:'20px'
+          paddingBottom:'35px'
         },
         cardPrice:{
             fontSize:32,
             fontWeight:700,
             lineHeight:'48px'
         },
-        cardCapitalize:{
-            color:`${colors.secondary.DEFAULT}`,
-            fontWeight:400,
-            fontSize:18,
-            lineHeight:'21px'
+        priceTrend:{
+            // color:`${colors.secondary.DEFAULT}`,
+            width:'20%',
+            display:'flex',
+            alignItems:'center',
+            padding:'2px',
+            // backgroundColor:'#A9FFA7',
+            borderRadius:4,
             },
-        topCardItem:{
-            backgroundColor:`${theme.palette.mode==="light"?colors.primary.DEFAULT:colors.primary[600]}`,
-            padding:'20px 16px',
-            minHeight:185,
-            border:`1px solid ${colors.borderColor}`,
-            borderRadius:12
-        }
+        trendUp:{
+            backgroundColor:'#A9FFA7',
+            color:'#037400'
+        },
+        trendDown:{
+            backgroundColor:'#ffa7a7',
+            color:'#740000'
+        },
+
+        // topCardItem:{
+        //     backgroundColor:`${theme.palette.mode==="light"?colors.primary.DEFAULT:colors.primary[600]}`,
+        //     padding:'20px 16px',
+        //     minHeight:185,
+        //     border:`1px solid ${colors.borderColor}`,
+        //     borderRadius:12
+        // }
     }
 })
